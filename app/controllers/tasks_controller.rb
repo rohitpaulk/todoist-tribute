1c10436e50ffc701bf75882ee974e4630df08b73
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-07-17 06:25:36 +0530

    add create/index for tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,26 @@
+class TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token # API, no CSRF required.
+
+  def index
+    render json: Task.all
+  end
+
+  def create
+    title = params[:title]
+    sort_order = params[:sort_order]
+
+    unless title
+      render json: {'msg' => 'title param missing'}, status: 400 and return
+    end
+
+    unless sort_order
+      render json: {'msg' => 'sort_order param missing'}, status: 400 and return
+    end
+
+    task = Task.create!(title: title, sort_order: sort_order)
+
+    # TODO: Catch sort_order uniqueness error
+
+    render json: task
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'api/v1/tasks', to: 'tasks#index'
+  post 'api/v1/tasks', to: 'tasks#create'
 end
