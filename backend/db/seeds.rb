f2dbfa6b8496630984dab36d8470879b173b406c
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-07-18 07:58:37 +0530

    Add seeds for tasks in backend

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating tasks..."
+
+Task.create!(sort_order: 1, title: 'This is the first task')
+Task.create!(sort_order: 2, title: 'da second task is here')
+Task.create!(sort_order: 3, title: '3rd task yo')
+
+puts "Tasks created."
