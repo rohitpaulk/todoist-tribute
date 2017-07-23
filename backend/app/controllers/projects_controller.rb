3a9511bc914f01a40da5834c0ffce5fa0d5d3cef
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-07-24 00:58:27 +0530

    Allow projects to be reordered on the backend

--- backend/app/controllers/projects_controller.rb
+++ backend/app/controllers/projects_controller.rb
@@ -2,4 +2,16 @@ class ProjectsController < ApplicationController
   def index
     render json: Project.all
   end
+
+  def reorder
+    project_ids = params[:project_ids]
+
+    unless project_ids
+      render json: {'msg' => 'task_ids param missing'}, status: 400 and return
+    end
+
+    Project.reorder!(project_ids)
+
+    render json: Project.all
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  match '*any_path', to: 'cors#preflight', via: :options
+
   get 'api/v1/tasks', to: 'tasks#index'
   post 'api/v1/tasks', to: 'tasks#create'
   post 'api/v1/tasks/reorder', to: 'tasks#reorder'
   put 'api/v1/tasks/:task_id', to: 'tasks#update'
-  match 'api/v1/tasks/reorder', to: 'cors#preflight', via: :options
-  match 'api/v1/tasks', to: 'cors#preflight', via: :options
-  match 'api/v1/tasks/:id', to: 'cors#preflight', via: :options
 
   get 'api/v1/projects', to: 'projects#index'
+  post 'api/v1/projects/reorder', to: 'projects#reorder'
 end
