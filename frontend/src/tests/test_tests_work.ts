5f060415bc7db20e8ffd0ea8a58ac0c409bdcaab
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-07-26 15:26:05 +0530

    Write a real test

--- frontend/src/tests/test_tests_work.ts
+++ frontend/src/tests/test_tests_work.ts
@@ -1,9 +1,12 @@
 import * as assert from 'assert';
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+import { Constructors, EditorNode } from '../helpers/editor_nodes';
+import { Project } from '../models';
+
+describe('Constructors  - projectPillNodeFromProject', function() {
+    it('should return project in data', function() {
+        let project: Project = {name: "Testing", id: '1', colorHex: "000000"};
+        let projectPillNode = Constructors.projectPillNodeFromProject(project);
+        assert.equal(project, projectPillNode.data.project);
     });
-  });
 });
