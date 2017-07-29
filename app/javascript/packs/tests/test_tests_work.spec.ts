86274b9551ed8488cf428f230af006bc6916c3d4
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-07-30 01:26:35 +0530

    Restore tests

--- app/javascript/packs/tests/test_tests_work.spec.ts
+++ app/javascript/packs/tests/test_tests_work.spec.ts
@@ -1,12 +1,13 @@
-// import * as assert from 'assert';
+import assert = require('assert');
+import 'mocha';
 
-// import { Constructors, EditorNode } from '../helpers/editor_nodes';
-// import { Project } from '../models';
+import { Constructors, EditorNode } from '../helpers/editor_nodes';
+import { Project } from '../models';
 
-// describe('Constructors  - projectPillNodeFromProject', function() {
-//     it('should return project in data', function() {
-//         let project: Project = {name: "Testing", id: '1', colorHex: "000000"};
-//         let projectPillNode = Constructors.projectPillNodeFromProject(project);
-//         assert.equal(project, projectPillNode.data.project);
-//     });
-// });
+describe('Constructors  - projectPillNodeFromProject', function() {
+    it('should return project in data', function() {
+        let project: Project = {name: "Testing", id: '1', colorHex: "000000"};
+        let projectPillNode = Constructors.projectPillNodeFromProject(project);
+        assert.equal(project, projectPillNode.data.project);
+    });
+});
