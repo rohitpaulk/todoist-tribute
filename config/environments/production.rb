ae66427ccb911e1633abb9b207f4ed591216176c
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-08-23 15:32:43 +0530

    Disable colorized logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.colorize_logging = false
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
