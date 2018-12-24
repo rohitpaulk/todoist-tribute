bd69f2020a2f5964294379f5aaa2ca1a1a5e496a
Paul Kuruvilla <rohitpaulk@gmail.com>
2018-12-25 02:42:01 +0530

    Add new webpacker config files

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,22 +1,5 @@
-// Note: You must restart bin/webpack-dev-server for changes to take effect
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-/* eslint global-require: 0 */
+const environment = require('./environment')
 
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const CompressionPlugin = require('compression-webpack-plugin')
-const sharedConfig = require('./shared.js')
-
-module.exports = merge(sharedConfig, {
-  output: { filename: '[name]-[chunkhash].js' },
-  devtool: 'source-map',
-  stats: 'normal',
-
-  plugins: [
-    new CompressionPlugin({
-      filename: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/
-    })
-  ]
-})
+module.exports = environment.toWebpackConfig()
